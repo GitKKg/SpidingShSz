a41e3a2e62e674f82dee5e0596708c66595bfcc9
GitKKg <lancexk@gmail.com>
2020-03-26 14:42:47 +0800

    Some customization use magit to create repository on github: H(Magithub) c(Create on GitHub) M(Remote) a(Add) : specify remote name and this new repository address in github (i.e. https://github.com/GitKKg/SpidingShSz)

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
-import Lib
+import SpidePrice
+
+-- use magit to create repository on github:
+-- H(Magithub) c(Create on GitHub)
+-- M(Remote) a(Add) : specify remote name and this new repository address in github
+-- (i.e. https://github.com/GitKKg/SpidingShSz)
 
 main :: IO ()
 main = someFunc
