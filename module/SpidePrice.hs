4d7cd0d6d7724d1cdfea111bf7c4b5b3724a133b
GitKKg <lancexk@gmail.com>
2020-03-26 16:26:59 +0800

    update dependencies of package.yaml and extra-deps of stack.yaml to import modules needed

--- module/SpidePrice.hs
+++ module/SpidePrice.hs
@@ -1,6 +1,55 @@
+{-# LANGUAGE OverloadedStrings ,DuplicateRecordFields #-}
+
 module SpidePrice
     ( someFunc
     ) where
+
+import Text.HTML.Scalpel
+import Control.Applicative
+
+import Network.HTTP.Client
+
+import qualified Data.ByteString.Lazy.Char8 as L8
+
+--import           Network.HTTP.Simple
+
+import Text.HTML.TagSoup
+
+import Network.Connection
+import Network.Socket
+import Network.HTTP.Client.TLS
+
+import Network.HTTP.Types.Status (statusCode)
+-- Scraper is just like a converting format  from some input to output
+-- Selector is just like a selecting format to select DOM node
+-- chroot accept an old Scraper and output a new Scraper,because it add a range by Selector for old Scraper
+
+import Control.Exception
+import Data.Either
+import Data.Typeable
+
+import Network.HTTP.Types.Header
+import Data.CaseInsensitive
+
+import Data.Text.Encoding
+
+import Data.Maybe
+
+--import Db -- Db.hs in the same directory,could import directly
+
+import Text.Regex.Base.RegexLike
+import Text.Regex.Posix.String
+
+import Data.Functor
+
+import Data.Text
+
+
+import qualified Data.Text.Encoding as T       -- text
+import qualified Data.Text.IO as T
+import qualified Data.Text as T
+import qualified Data.Text.ICU.Convert as ICU  -- text-icu   ,proxychains stack install text-icu
+import qualified Data.Text.ICU as ICU
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
