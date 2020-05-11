791c5bc733593eccc1671b0fefbed19f825101c5
GitKKg <lancexk@gmail.com>
2020-05-11 18:02:45 +0800

    concurrent spiding intial draft

--- app/Main.hs
+++ app/Main.hs
@@ -22,7 +22,12 @@ import Data.Either
 
 import ConcurrentUtility
 
-main :: IO ()
+import System.Directory
+import System.IO
+import System.Environment
+
+import Debug.Trace
+import Control.Monad.Trans
 
 type StartYear = Int
 type StartSeason = Int
@@ -48,7 +53,7 @@ type Season = Int
 getYSList :: StartYear -> StartSeason -> EndYear -> EndSeason -> IO [(Year,Season)] 
 getYSList y1 s1 y2 s2 = do
   cy <- currentYear
-  if (y1 > y2 || y1 < 2002 || y2 > cy)
+  if y1 > y2 || y1 < 2002 || y2 > cy
   then error "Wrong year range!must limited between 2002 and now!"
   else
     return . reverse $ filter (not. (\(y,s) -> y == y1 && s <s1 || y == y2 && s >s2 )) ysl where
@@ -81,8 +86,47 @@ grsDemo = do
   
 -- MVar [(code,year,season)]
 -- one thread get one mp, Maybe PortNumber, take one (code,year,season) from head of MVar List,then putMVar tail of List,so ,these multi-thread cocurrent like this way,when head List is empty,thread putMVar exit to notify main it is ok,when all threads are ok,main ok,out 
-
-main = testMvList--someFunc
+--wmain :: StartYear -> StartSeason -> EndYear -> EndSeason -> FilePath -> IO()
+--logOut :: String -> IO ()
+logOut log = (>>) <$> liftIO . (appendFile log) <*> traceM
+main = do --testMvList--someFunc
+  args <- getArgs
+  let sy = read @StartYear $ args!!0
+  traceM $ show sy
+  let ss = read @StartSeason $ args!!1
+  traceM $ show ss
+  let ey = read @EndYear $ args!!2
+  traceM $ show ey
+  let es = read @EndSeason $ args!!3
+  traceM $ show es
+  let fp = args!!4 -- "./module/sinaCodes"--read @String $ args!!4
+  traceM $ fp
+  cysList <- getCYSList fp sy ss ey es
+  (>>=) <$> doesDirectoryExist <*>  ((flip when) . removeDirectoryRecursive)  $ "./log"
+  createDirectoryIfMissing True "./log"
+  syncM <- newEmptyMVar
+  mlist <- newMVar cysList
+  let threadList = portList
+  mapM (forkIO . threadWork syncM mlist) threadList
+  mapM (\_ -> takeMVar syncM) threadList
+  print "main over"
+  where
+    threadWork syncM mlist mayPort = do
+      id <- myThreadId
+      let log = "./log/" ++ ((!! 1) . words . show) id ++ "log.txt"
+      fileExist <- doesFileExist $ log
+      when (not fileExist) $ openFile log WriteMode >>= hClose
+      list <-takeMVar mlist
+      id <- myThreadId
+      if null list
+        then do
+        logOut log $ show id ++  " out for list is empty \n"
+        putMVar mlist list -- if not put back,other thread will block
+        putMVar syncM 'x'
+        else do
+        logOut log $ show id ++ " get " ++ (show . head) list ++ "\n"
+        putMVar mlist (tail list)
+        threadWork syncM mlist mayPort
 -- main = do
 --   print "input code path,start year,season,end year,season \n"
 --   fp <- read @String <$>  getLine
